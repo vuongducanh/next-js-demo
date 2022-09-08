dde31ae1bbc8a2cf2cab6ef454b008ac75c87281
vuongducanh <anhvuong730@gmail.com>
2022-09-08 13:46:05 +0700

    update index.js file

--- pages/post/[detail].tsx
+++ pages/post/[detail].tsx
@@ -1,4 +1,5 @@
 import { memo } from 'react';
+import { useRouter } from 'next/router'
 
 const PostDetail = memo((props: any) => {
   const { post } = props;
@@ -10,26 +11,38 @@ const PostDetail = memo((props: any) => {
     );
 });
 
-export async function getStaticPaths() {
-    // Call an external API endpoint to get posts
-    const res = await fetch('https://610b520052d56400176b0202.mockapi.io/api/production')
-    const posts = await res.json()
+// export async function getStaticPaths() {
+//     // Call an external API endpoint to get posts
+//     const res = await fetch('https://610b520052d56400176b0202.mockapi.io/api/production')
+//     const posts = await res.json()
 
-    // Get the paths we want to pre-render based on posts
-    const paths = posts.map((post: any) => ({
-      params: { detail: post.id },
-    }))
+//     // Get the paths we want to pre-render based on posts
+//     const paths = posts.map((post: any) => ({
+//       params: { detail: post.id },
+//     }))
 
-    // We'll pre-render only these paths at build time.
-    // { fallback: false } means other routes should 404.
-    return { paths, fallback: false }
-  }
+//     // We'll pre-render only these paths at build time.
+//     // { fallback: false } means other routes should 404.
+//     return { paths, fallback: false }
+//   }
 
-  export async function getStaticProps({ params }: any) {
+  // export async function getStaticProps({ params }: any) {
+  //   // params contains the post `id`.
+  //   // If the route is like /posts/1, then params.id is 1
+  //   const res = await fetch(`https://610b520052d56400176b0202.mockapi.io/api/production/${params.detail}`)
+  //   const post = await res.json()
+
+  //   // Pass post data to the page via props
+  //   return { props: { post } }
+  // }
+
+  export async function getServerSideProps(context: any) {
+    const { detail } = context.query;
+
     // params contains the post `id`.
     // If the route is like /posts/1, then params.id is 1
-    const res = await fetch(`https://610b520052d56400176b0202.mockapi.io/api/production/${params.detail}`)
-    const post = await res.json()
+    const res = await fetch(`https://610b520052d56400176b0202.mockapi.io/api/production/${detail}`)
+    const post = await res.json();
 
     // Pass post data to the page via props
     return { props: { post } }
